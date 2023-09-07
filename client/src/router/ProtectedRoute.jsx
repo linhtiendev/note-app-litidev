014084ba1980e53c8f95851c98bc32d2b8173f00
linhtiendev <lelinhtien0707@gmail.com>
2023-09-07 18:38:45 +0700

    create AuthContext to store user information

---
+++ client/src/router/ProtectedRoute.jsx
@@ -0,0 +1,14 @@
+// xác nhận user đã login chưa
+import React from 'react'
+import { Outlet, useNavigate } from 'react-router-dom'
+
+export default function ProtectedRoute(children) {
+    const navigate = useNavigate();
+    if (!localStorage.getItem('accessToken')){
+        navigate("/login")
+        return;
+    }
+  return (
+    <Outlet />
+  )
+}
--- client/src/router/index.jsx
+++ client/src/router/index.jsx
@@ -1,23 +1,32 @@
 import { Outlet, createBrowserRouter } from 'react-router-dom'
 import Login from '../pages/Login'
 import Home from '../pages/Home'
+import AuthProvider from '../context/AuthProvider'
+import ProtectedRoute from './ProtectedRoute'
+import ErrorPage from '../pages/ErrorPage'
 
 const AuthLayout = () => {
     // Oulet -> sẽ render 1 trong những children dựa vào path
-    return <Outlet />
+    return <AuthProvider><Outlet /></AuthProvider>
 }
 
 export default  createBrowserRouter([
     {
         element: <AuthLayout />,
+        errorElement: <ErrorPage />, // hiện khi dùng dường dẫn chưa định nghĩa
         children: [
             {
                 element: <Login />,
                 path: "/login",
             },
             {
-                element: <Home />,
-                path: "/",
+                element: <ProtectedRoute />,
+                children: [
+                    {
+                        element: <Home />,
+                        path: "/",
+                    }
+                ]
             }
         ]
     }
